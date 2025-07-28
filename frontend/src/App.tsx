34519b9d6311d6b361348470d95dfeb41e81b17c
ianfelps <iannfelps@gmail.com>
2025-07-28 11:11:52 -0300

    Start loggin and register implementation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css';
 import { Outlet } from 'react-router';
 import Navbar from './Components/Navbar/Navbar';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <>
       <Navbar />
       <Outlet />
+      <ToastContainer />
     </>
   );
 }
